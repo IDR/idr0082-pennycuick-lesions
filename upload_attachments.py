acbd94d5416bfe253bb9f0ae8fda1d55265c1899
Petr Walczysko <p.walczysko@dundee.ac.uk>
2020-05-07 10:41:17 +0100

    Improve script structure and variable names

--- upload_attachments.py
+++ upload_attachments.py
@@ -17,8 +17,8 @@ from omero_upload import upload_ln_s
 project_id = 1052 # 952
 attachment_file = "attachments.txt"    # "/tmp/attachments.txt"
 link_to_image = True
-image_ending = False
-image_matches = False
+image_ending_right = False
+image_matches_attachment = False
 
 OMERO_DATA_DIR = '/data/OMERO'
 NAMESPACE = 'openmicroscopy.org/idr/analysis/original'
@@ -57,30 +57,24 @@ def process_line(conn, project, line, link_image, count):
     # /uod/idr/filesets/idr0082-pennycuick-lesions/20200417-ftp/S2_HandE.ndpi.ndpa
     parts = line.split('/')
     att_name_parts = parts[6].split('.')
-    dataset_name = "Desktop"
+#    dataset_name = "Desktop"
 
     datasets = []
     for dataset in project.linkedDatasetList():
-        print(dataset_name)
+        print(dataset.getName().getValue())
         if link_image:
             for image in dataset.linkedImageList():
                 image_name_space = image.getName().getValue().split(' ')
                 length = len(image_name_space)
-                if (image_name_space[length-1] == '[0]'):
-                    image_ending = True
-                else:
-                    image_ending = False
+                image_ending_right = (image_name_space[length-1] == '[0]')
                 image_name_dot = image.getName().getValue().split('.')
-                if (att_name_parts[0] == image_name_dot[0]):
-                    image_matches = True
-                else:
-                    image_matches = False
-                if (image_ending and image_matches):
+                image_matches_attachment = (att_name_parts[0] == image_name_dot[0])
+                if (image_ending_right and image_matches_attachment):
                     link(conn, image, line, link_image)
                     print("Linked attachment %s to image %s" %
                             (line, image.getName().getValue()))
-        elif dataset.getName().getValue().startswith(dataset_name):
-            datasets.append(dataset)
+#        elif dataset.getName().getValue().startswith(dataset_name):
+#            datasets.append(dataset)
 
     if len(datasets) > 0:
         link(conn, datasets, line, link_image)
